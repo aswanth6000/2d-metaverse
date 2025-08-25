4d9a174c3286949bdfd324d464e4722e59d54a6a
aswanth6000 <aswanth682@gmail.com>
2025-08-25 17:13:53 +0530

    contoroller with route setup

---
+++ apps/server/http/src/controllers/auth.controller.ts
@@ -0,0 +1,29 @@
+
+const login = async (req, res) => {
+
+}
+
+const register = async (req, res) => {
+
+}
+
+const refresh = async (req, res) => {
+
+}
+
+const logout = async (req, res) => {
+
+}
+
+const me = async (req, res) => {
+
+}
+
+
+export const authController = {
+    login,
+    register,
+    refresh,
+    logout,
+    me
+}
--- apps/server/http/src/routes/v1/auth.routes.ts
+++ apps/server/http/src/routes/v1/auth.routes.ts
@@ -3,4 +3,4 @@ import { Router } from "express";
 const router = Router()
 
 
-// router.route()
+router.route('/login').post()
